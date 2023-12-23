1e6ef9dc74d9168ed038cfc52a14503439839889
baorii <john.guibao127@gmail.com>
2023-12-23 18:30:17 +0000

    Added map module file and roadmap of features needed.

---
+++ PinPointApp/map.js
@@ -0,0 +1,22 @@
+function show_map() {
+    //display map on click of button?
+}
+
+function current_location() {
+    //gets precise current geo-location
+}
+
+var location_now = () => {
+    //call current_location
+    //shows this location on map
+}
+
+//maybe call this when current_location is different to location_now
+function display_path(start, end) {
+    //show path on map from start node to end node
+}
+
+
+const map_zoom = () => {
+    //set default zoom level
+}
