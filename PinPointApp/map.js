b515c886fbda07fa2dd31c08649cc573330c74af
baorii <john.guibao127@gmail.com>
2023-12-29 14:39:59 +0000

    Added necessary library imports and set up show_map function

--- PinPointApp/map.js
+++ PinPointApp/map.js
@@ -1,5 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+import MapView, {Marker} from 'react-native-maps';
+
+const [location, setLocation] = useState(); //location held in state variable for easier updating
+const [address, setAddress] = useState();
+
+const [mapRegion, setMapRegion] = useState();//map view
+
 function show_map() {
     //display map on click of button?
+
+    setMapRegion({
+        longitude: location.coords.longitude,
+        latitude: location.coords.latitude,
+        longitudeDelta: 0.0922,
+        latitudeDelat: 0.0421,
+      });//map sets current location
+      
+    return ( // visual section of app
+    <View style={styles.container}>
+      <Text>Map</Text> 
+      
+      <MapView style={styles.map} 
+        region={mapRegio}
+      >
+        <Marker coordinate={mapRegio} title='Marker' />
+      </MapView>
+      
+    </View>
+  );
 }
 
 function current_location() {
@@ -20,3 +51,18 @@ function display_path(start, end) {
 const map_zoom = () => {
     //set default zoom level
 }
+
+
+//testing purposes - can add to actual css file later
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: '50%',
+      height: '50%',
+    },
+  });
